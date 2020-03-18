8b3fadda748e14fb4011d59b511de9c894009a8d
Omer Geva <ogeva97@gmail.com>
2020-03-18 08:14:55 +0200

    update menu-item to withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,10 +8,21 @@ import './pages/homepage/homepage.styles.scss';
 
 import Homepage from './pages/homepage/homepage.component';
 
+const Hats = () => (
+  <div>
+    <h1>
+      HATS
+    </h1>
+  </div>
+  );
+
 function App() {
   return <div>
-    <Homepage />;
-  </div>;
+    <Switch>
+      <Route exact path='/' component={Homepage} />
+      <Route exact path='/hats' component={Hats} />
+    </Switch>
+  </div>
 }
 
 export default App;
