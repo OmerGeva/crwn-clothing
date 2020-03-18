6d9de1d3acb39d963c9e44bbe4fb14a6852c678a
Omer Geva <ogeva97@gmail.com>
2020-03-18 16:50:57 +0200

    added navbar (header)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import logo from './logo.svg';
 import './App.css';
 
 import './pages/homepage/homepage.styles.scss';
 
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component'
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 function App() {
   return <div>
+    <Header />
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route exact path='/shop' component={ShopPage} />
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { ReactComponent as Logo } from '../../assets/crown.svg'
+
+import { Link } from 'react-router-dom';
+import './header.styles.scss';
+
+
+const Header = () => (
+  <div className="header">
+    <Link className='logo-container' to="/" >
+     <Logo className="logo"></Logo>
+    </Link>
+    <div className="options">
+      <Link class="option" to='/shop'>
+        SHOP
+      </Link>
+      <Link class="option" to='/shop'>
+        CONTACT
+      </Link>
+    </div>
+  </div>
+  );
+
+export default Header;
